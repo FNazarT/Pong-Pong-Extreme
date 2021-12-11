d14abe0f90427ef7f00c1202466a4f3f3690d275
Francisco Nazar <francisco.nazartorres@gmail.com>
2021-12-11 17:01:57 -0300

    First Commit
    Complete Project.
    
    Credits: Tutorial from Thomas Brush's YouTube Channel

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class Ball : MonoBehaviour
+{
+    public float speedMultiplier;
+    public float speedMultiplierOrig;
+    private Vector2[] randomVelocity = new Vector2[] { new Vector2(0.5f, 0.5f), new Vector2(-0.5f, 0.5f), new Vector2(0.5f, -0.5f), new Vector2(-0.5f, -0.5f)};
+    public Vector2 velocity;
+    public Animator uiAnimator;
+    public TextMeshProUGUI uiText;
+    public Player player;
+    public AudioClip hitSound;
+    public AudioClip dieSound;
+    public AudioSource audioSource;
+    public AudioSource music;
+
+    void Start()
+    {
+       speedMultiplierOrig = speedMultiplier;
+       velocity = randomVelocity[Random.Range(0, 4)];
+    }
+
+    void Update()
+    {
+        transform.position += new Vector3(velocity.x, velocity.y) * Time.deltaTime * speedMultiplier;
+        transform.RotateAround(transform.position, transform.forward, Time.deltaTime * speedMultiplier * 100);
+
+        if(transform.position.x < -10 || transform.position.x > 10 || transform.position.y > 5 || transform.position.y < -5)
+        {
+            ResetLevel();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Vertical Paddle" || col.gameObject.name == "Horizontal Paddle")
+        {
+            if (col.gameObject.name == "Vertical Paddle")
+            {
+                velocity.x = -velocity.x;
+            }
+
+            if (col.gameObject.name == "Horizontal Paddle")
+            {
+                velocity.y = -velocity.y;
+            }
+
+            uiAnimator.SetTrigger("ScoreEffect");
+            player.score++;
+            uiText.text = player.score.ToString();
+            speedMultiplier += .1f;
+            audioSource.pitch = Random.Range(.8f, 1.3f);
+            audioSource.PlayOneShot(hitSound);
+        }
+    }
+
+    void ResetLevel()
+    {
+        music.Stop();
+        transform.position = Vector2.zero;
+        player.score = 0;
+        uiText.text = "OOPS!";
+        velocity = randomVelocity[Random.Range(0, 4)];
+        speedMultiplier = speedMultiplierOrig;
+        audioSource.PlayOneShot(dieSound);
+        music.Play();
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Vector3 mousePosition;
+    [SerializeField] private Camera twoDimensionalCamera;
+    [SerializeField] private GameObject horizontalPaddle;
+    [SerializeField] private GameObject verticalPaddle;
+    [SerializeField] LayerMask horizontalLayerMask;
+    [SerializeField] LayerMask verticalLayerMask;
+    public int score = 0;
+
+    void FixedUpdate()
+    {
+        // Check floor and ceiling
+        RaycastHit2D hitFloors = Physics2D.Raycast(transform.position, transform.right, 20, horizontalLayerMask);
+        Debug.DrawRay(transform.position, transform.right*20, Color.green);
+
+        //If something was hit, the RaycastHit2D.collider will not be null.
+        if (hitFloors.collider != null)
+        {
+            horizontalPaddle.transform.position = hitFloors.point;
+        }
+
+        // Check walls
+        RaycastHit2D hitWalls = Physics2D.Raycast(transform.position, transform.right, 20, verticalLayerMask);
+        Debug.DrawRay(transform.position, transform.right * 20, Color.red);
+
+        //If something was hit, the RaycastHit2D.collider will not be null.
+        if (hitWalls.collider != null)
+        {
+            verticalPaddle.transform.position = hitWalls.point;
+        }
+
+        // Rotate to mouse
+        mousePosition = Input.mousePosition;
+
+        Vector3 objectPos = twoDimensionalCamera.WorldToScreenPoint(transform.position);
+        mousePosition.x -= objectPos.x;
+        mousePosition.y -= objectPos.y;
+
+        float angle = Mathf.Atan2(mousePosition.y, mousePosition.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+}
